c50b68a0a0075eb0d75633bccff1b815a6ecdbcd
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2023-01-14 12:47:58 +0530

    Method overriding is done

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,20 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        System.out.println(super.meth());
+        return "Invoking method from class B";
+    }
+}
 public class TestCases {
-    
+    public static void main(String[] args) {
+        B obj=new B();
+        System.out.println(obj.meth());
+    }
 }
