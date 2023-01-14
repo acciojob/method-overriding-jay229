064acc1960d1e4d1d1e569df3fcb1c73aa4b3d7d
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2023-01-14 12:22:43 +0530

    first assignment is completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
-
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        System.out.println(super.meth());
+        return "Invoking method from class B";
+    }
+}
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj=new B();
+        System.out.println(obj.meth());
+    }
 }
